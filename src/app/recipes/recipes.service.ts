8c2e6442436db2ceb33052996a9b3316392f6f89
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2019-01-03 00:10:37 +0530

    Added ingredients to the recipe model

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,6 +1,7 @@
 import { EventEmitter } from '@angular/core';
 
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipesService {
 
@@ -9,8 +10,25 @@ export class RecipesService {
   private recipes: Recipe[] = [
     new Recipe(
       'Pani Puri',
-      'Pani puri recipe',
-      'http://cdn1.foodviva.com/static-content/food-images/snacks-recipes/pani-puri/pani-puri.jpg'
+      'Pani puri is a tangy, spicy as ell as sweet food. It is one of the most beloved street food in India.',
+      'http://cdn1.foodviva.com/static-content/food-images/snacks-recipes/pani-puri/pani-puri.jpg',
+      [
+        new Ingredient('Potato', 3),
+        new Ingredient('Onion', 1),
+        new Ingredient('Mint', 20),
+        new Ingredient('Green Chilli', 8)
+      ]
+    ),
+    new Recipe(
+      'Pav Bhaji',
+      'Pav Bhaji is an iconic dish from Mumbai and famous all over India',
+      'https://www.merisaheli.com/wp-content/uploads/2016/11/pav-bhaji-e1528959679489.jpg',
+      [
+        new Ingredient('Potato', 3),
+        new Ingredient('Onion', 2),
+        new Ingredient('Tomato', 2),
+        new Ingredient('Butter', 1)
+      ]
     )
   ];
 
