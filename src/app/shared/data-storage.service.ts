576c00fd87970c0b74346fb34585c01a38b51e35
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2019-01-25 23:59:40 +0530

    Managing the absense of ingredients array from server data

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,8 @@
+import { Recipe } from './../recipes/recipe.model';
 import { RecipesService } from './../recipes/recipes.service';
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class DataStorageService {
@@ -12,9 +14,19 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-    this.http.get('https://atharv-recipe-book.firebaseio.com/recipes.json').subscribe(
-      (response: Response) => {
-        const recipes = response.json();
+    this.http.get('https://atharv-recipe-book.firebaseio.com/recipes.json')
+      .pipe(
+        map((response: Response) => {
+          const recipes: Recipe[] = response.json();
+          for (const recipe of recipes) {
+            if (!recipe['ingredients']) {
+              recipe['ingredient'] = [];
+            }
+          }
+          return recipes;
+        })
+      ).subscribe(
+      (recipes: Recipe[]) => {
         this.recipesService.setRecipes(recipes);
       }
     );
