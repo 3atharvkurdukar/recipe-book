2ffbd5ae58264bb135e927bd5381ebf4be0a2fde
Atharv Kurdukar <3atharvkurdukar@gmail.com>
2019-01-31 15:17:29 +0530

    Comleting login using firebase authentication

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,4 +5,9 @@ export class AuthService {
       firebase.auth().createUserWithEmailAndPassword(email, password)
         .catch(error => console.log(error));
     }
+    loginUser(email: string, password: string) {
+      firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(response => console.log(response))
+        .catch(error => console.log(error));
+    }
 }
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,7 @@
+import { AuthService } from './../auth.service';
+import { NgForm } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+  }
+
+  onLogin(form: NgForm) {
+    const email = form.value.email;
+    const password = form.value.password;
+    this.authService.loginUser(email, password);
   }
 
 }
